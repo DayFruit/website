dbb555aae089b22ca6ed1ebbb311dc7e67caf944
dayfruit <160675784+DayFruit@users.noreply.github.com>
2024-02-20 16:47:08 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script for the CyberSec website
+
+// Get the learn more button
+var learnMore = document.getElementById('learn-more');
+
+// Add a click event listener to the button
+learnMore.addEventListener('click', function() {
+    // Redirect the user to the about page
+    window.location.href = 'about.html';
+});
